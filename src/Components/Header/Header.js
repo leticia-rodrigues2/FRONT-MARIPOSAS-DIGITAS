914f48a23b04d3d4ae4c0a132056cb6887cbef1a
Leticia Rodrigues Bento <leticiaarodrigues2@gmail.com>
2024-04-30 23:54:19 -0300

    header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="images//LogoText.png" alt="Logo" className="logo" />
+    </header>
+  );
+};
+
+export default Header;
--- src/Pages/ResetPassword/ResetPassword.js
+++ src/Pages/ResetPassword/ResetPassword.js
@@ -3,6 +3,7 @@ import TextField from '@mui/material/TextField';
 import Link from '@mui/material/Link';
 import { useNavigate } from "react-router-dom";
 import Conteiner from "../../Components/Conteiner/Conteiner.js";
+import Header from "../../Components/Header/Header.js"; 
 import "./ResetPassword.css";
 
 function ResetPassword() {
@@ -44,22 +45,25 @@ function ResetPassword() {
   };
 
   return (
-    <Conteiner
-      titulo="REDEFINA SUA SENHA"
-      paragrafo
-      textoBotao="ALTERAR SENHA"
-      textoLink
-      link="/cadastro"
-    >
-    
-     
-      <div className="input-row">
-        <TextField id="password" label="Insira sua nova senha" variant="outlined" defaultValue="Normal" size="small" color="secondary"  fullWidth={true} value={email} onChange={handleEmailChange} />
-      </div>
-      <div className="input-row">
-        <TextField id="password" label="Confirme sua senha" variant="outlined" defaultValue="Normal" size="small"color="secondary" fullWidth={true} type="password" value={password} onChange={handlePasswordChange} /> {/* Definindo o tipo como 'password' */}
-      </div>
-    </Conteiner>
+    <>
+      <Header /> {/* Adicione o componente Header */}
+      <Conteiner
+        titulo="REDEFINA SUA SENHA"
+        paragrafo
+        textoBotao="ALTERAR SENHA"
+        textoLink
+        link="/cadastro"
+      >
+      
+       
+        <div className="input-row">
+          <TextField id="password" label="Insira sua nova senha" variant="outlined" defaultValue="Normal" size="small" color="secondary"  fullWidth={true} value={email} onChange={handleEmailChange} />
+        </div>
+        <div className="input-row">
+          <TextField id="password" label="Confirme sua senha" variant="outlined" defaultValue="Normal" size="small"color="secondary" fullWidth={true} type="password" value={password} onChange={handlePasswordChange} /> {/* Definindo o tipo como 'password' */}
+        </div>
+      </Conteiner>
+    </>
   );
 }
 
